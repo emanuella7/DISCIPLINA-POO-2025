74390a0f80bd6cf0924ddd20f5ce8f06a52bb8ca
João Victor <141837958+Jotinha085@users.noreply.github.com>
2025-04-02 12:46:23 -0300

    Add files via upload

---
+++ LISTA-01/EXE-RESOLVIDOS/Q09/src/br/edu/principal/Principal.java
@@ -0,0 +1,28 @@
+package br.edu.principal;
+import java.util.Scanner;
+public class Principal {
+//EXE RESOLVIDOS
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.print("Digite o valor da base do triângulo: ");
+        double base = scanner.nextDouble();
+
+        
+        System.out.print("Digite o valor da altura do triângulo: ");
+        double altura = scanner.nextDouble();
+
+        
+        double area = (base * altura) / 2;
+
+       
+        System.out.printf("A área do triângulo é: %.2f\n", area);
+
+        
+        scanner.close();
+    }
+
+	}
+
+
